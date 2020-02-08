9e6a18b2e94a54354d5a1ebbcd3bf66b409cf0ad
Aditya kolhatkar <adityakolhatkar7867@gmail.com>
2020-02-08 12:37:57 +0530

    Aditya Kolhatkar- Java Workshop

---
+++ src/aditya/FirstDemo.java
@@ -0,0 +1,22 @@
+package aditya;
+
+public class FirstDemo {                                     // syntax for cretaiog class
+	
+	
+	void showData() {
+		System.out.printf("Hello");
+		
+	}
+	
+	
+	public static void main(String[] args) {                               // class 
+		
+		FirstDemo demo=new FirstDemo();           // syntax for creating the object
+		
+		demo.showData();
+		System.out.printf("Hello");                              // print statment in java
+		
+		
+	}
+
+}
